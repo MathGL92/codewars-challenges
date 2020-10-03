9944fbb6796dcac267db9a6311f2f2d08637c273
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-03 12:10:28 +1000

    challenge solved, all TDD passed

--- pagination-helper/pagination_helper.rb
+++ pagination-helper/pagination_helper.rb
@@ -16,7 +16,7 @@ class PaginationHelper
 
   # returns the number of pages
   def page_count
-    (item_count / @items_per_page.to_f).round
+    (item_count / @items_per_page.to_f).ceil
   end
 
   # returns the number of items on the current page. page_index is zero based.
@@ -24,23 +24,27 @@ class PaginationHelper
   def page_item_count(page_index)
     # input page index
     # array of pages : transform page_count to array of page_count elements
-    pages_array = Array.new(page_count) {|i| i }
-    if pages_array[page_index].nil?
-      -1
-    else
-      if pages_array[page_index] != pages_array[-1]
-        @items_per_page
-      else
-        item_count % @items_per_page
-      end
-    end
+    pages_array = Array.new(page_count) { |i| i }
     # return the number of item on page_index if page_index in the range else return -1
+    if pages_array[page_index].nil? || page_index < 0
+      -1
+    elsif pages_array[page_index] != pages_array[-1]
+      @items_per_page
+    else
+      item_count % @items_per_page
+    end
   end
 
   # determines what page an item is on. Zero based indexes.
   # this method should return -1 for item_index values that are out of range
   def page_index(item_index)
     # input item index
-    # return the page index if item in the range else return -1
+    # guard clause to check item in is the range
+    if @collection[item_index].nil? || item_index < 0
+      -1
+    else
+      # return the page index if item in the range else return -1
+      item_index / @items_per_page
+    end
   end
 end
